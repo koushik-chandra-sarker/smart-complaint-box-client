2cda6b74ec147bc7c7fda930a553f2451a4dbfe3
koushik-chandra-sarker <koushiksk.ks@gmail.com>
2023-08-16 22:34:13 +0600

    privacy-policy

---
+++ src/app/privacy-policy/page.js
@@ -0,0 +1,105 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            <div className={"container m-auto  w-full md:px-10 px-5 md:w-8/12 w-11/12 my-10 p-5 rounded-xl bg-primary-200 "}>
+                <h1 className={"font-bold text-2xl text-center"}>Privacy Policy for "স্মার্ট অভিযোগ বক্স"</h1>
+                <br/>
+                Last Updated: 16/08/2023
+                <br/>
+                <h1 className={"font-bold text-xl mt-5"}>1. Introduction</h1> <br/>
+
+                Welcome to "স্মার্ট অভিযোগ বক্স" , a platform developed by iAutoLabs we that allows students under the
+                Upazila to submit complaints to their authority, such as the Upazila Nirbahi Officer ("UNO"). This
+                privacy policy explains how we collect, use, disclose, and protect personal information when you use our
+                App. By using the App, you agree to the terms of this policy.
+
+                <h1 className={"font-bold text-xl mt-5"}>2. Information We Collect</h1> <br/>
+                <ol className={"list-decimal list-inside"}>
+                    <li> Complaint Information: When you use the App to submit a complaint, we collect the details you
+                        provide, including the nature of the complaint, date.
+                    </li>
+                    <li>Personal Information: We may collect your name, contact information (email address, phone
+                        number),
+                        and any information you choose to provide in user profiles or communications.
+                    </li>
+                    <li>Device Information: We may collect information about the device you use to access the App, such
+                        as
+                        device type, operating system version, and unique device identifiers.
+                    </li>
+                </ol>
+
+                <h1 className={"font-bold text-xl mt-5"}> 3. How We Use Your Information</h1> <br/>
+                <ol className={"list-decimal list-inside"}>
+                    <li>Process and Address Complaints: We use the information you provide to process and address your
+                        complaints, facilitate communication with relevant authorities, and track the status and
+                        resolution of
+                        complaints.
+                    </li>
+                    <li>Communication: We may use your contact information to update you on the progress of your
+                        complaint
+                        and to communicate important information related to the complaint management process.
+                    </li>
+                    <li>Improve the App: We use collected data to improve the functionality and user experience of the
+                        App,
+                        including identifying trends and areas for enhancement.
+                    </li>
+                    <li>Analytics: We may use analytics tools to analyze user behavior and app usage patterns. This
+                        information helps us optimize the App's performance and features.
+                    </li>
+                </ol>
+
+
+                <h1 className={"font-bold text-xl mt-5"}>4. Data Sharing and Disclosure</h1> <br/>
+                <ol className={"list-decimal list-inside"}>
+                    <li>Upazila Authorities: We may share complaint information and personal data with relevant
+                        authorities
+                        within the "Ashuganj Upazila" to address and resolve complaints effectively.
+                    </li>
+                    <li>Third-Party Service Providers: We may engage third-party service providers to assist with app
+                        development, hosting, analytics, and customer support. These providers may access your
+                        information
+                        but
+                        are bound by confidentiality obligations.
+                    </li>
+                    <li>Legal Requirements: We may disclose your information if required by law or in response to valid
+                        legal
+                        requests.
+                    </li>
+
+
+                </ol>
+                <h1 className={"font-bold text-xl mt-5"}> 5. Security:</h1> <br/>
+
+                We implement reasonable security measures to safeguard your personal information from
+                unauthorized
+                access, disclosure, alteration, or destruction.
+                <h1 className={"font-bold text-xl mt-5"}>6. Children's Privacy</h1> <br/>
+
+                The App is intended for use by individuals who can be student or their family member, relative,
+                others.
+                We do not knowingly collect personal information from children under 5[Five]. If you believe we have
+                collected such information, please contact us immediately.
+
+                <h1 className={"font-bold text-xl mt-5"}>7. Your Choices</h1> <br/>
+
+                You can review, update, or delete your personal information within the App's settings. You may also
+                contact us to request account deletion.
+
+                <h1 className={"font-bold text-xl mt-5"}>8. Changes to this Privacy Policy</h1> <br/>
+
+                We may update this privacy policy to reflect changes in our practices or for operational, legal, or
+                regulatory reasons. We will notify you of any material changes by posting the updated policy on our
+                website or within the App.
+
+                <h1 className={"font-bold text-xl mt-5"}>9. Contact Us</h1> <br/>
+
+                If you have questions, concerns, or requests regarding this privacy policy, please contact us at
+                support@smartavijogbox.com.
+            </div>
+        </div>
+    );
+};
+
+export default Page;
