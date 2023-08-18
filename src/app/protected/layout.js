daa5033891fddb3264fa21ccfd1dee5c4cf658ec
koushik-chandra-sarker <koushiksk.ks@gmail.com>
2023-08-18 22:15:58 +0600

    update redirect

--- src/app/protected/layout.js
+++ src/app/protected/layout.js
@@ -1,16 +1,15 @@
 "use client"
 import {useDispatch, useSelector} from "react-redux";
 import {handleTokenExpiryAndRefresh} from "@/redux/slices/authSlice";
-import {redirect, useRouter} from "next/navigation";
+import {redirect} from "next/navigation";
 
 export default function ProtectedLayout({children}) {
-    const router = useRouter()
     const dispatch = useDispatch();
     dispatch(handleTokenExpiryAndRefresh());
     const {isAuthenticated} = useSelector(state => state.auth);
 
     if (!isAuthenticated) {
-        router.push("/login")
+        redirect("/login")
     } else {
         return (
             <>
